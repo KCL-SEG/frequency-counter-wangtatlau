a123378cecaceee1df7cfb863ae6cf44fb3d25ed
Wang Tat Lau (k22023660) <wang.lau@kcl.ac.uk>
2023-09-26 00:07:15 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,9 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if str(item) in frequencies:
+            frequencies[str(item)] = frequencies.get(str(item)) + 1
+        else:
+            frequencies[str(item)] = 1
     return frequencies
